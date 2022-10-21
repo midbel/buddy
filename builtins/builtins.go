a8136c82053dedf358304915c733d58a6e996069
midbel <brun.nicolas21@gmail.com>
2022-10-21 12:01:55 +0200

    type conversion builtins

--- builtins/builtins.go
+++ builtins/builtins.go
@@ -2,8 +2,10 @@ package builtins
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/midbel/buddy/types"
+	"github.com/midbel/slices"
 )
 
 type BuiltinFunc func(...types.Primitive) (types.Primitive, error)
@@ -19,6 +21,10 @@ var Builtins = map[string]BuiltinFunc{
 	"incr":   Incr,
 	"decr":   Decr,
 	"exit":   Exit,
+	"int":    Int,
+	"float":  Float,
+	"string": String,
+	"bool":   Bool,
 }
 
 func Lookup(name string) (BuiltinFunc, error) {
@@ -27,4 +33,79 @@ func Lookup(name string) (BuiltinFunc, error) {
 		return nil, fmt.Errorf("%s: builtin not defined", name)
 	}
 	return b, nil
+}
+
+func Int(args ...types.Primitive) (types.Primitive, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("invalid number of arguments")
+	}
+	var (
+		raw = slices.Fst(args).Raw()
+		val int64
+	)
+	switch v := raw.(type) {
+	case int64:
+		val = v
+	case float64:
+		val = int64(v)
+	case string:
+		x, err := strconv.ParseInt(v, 0, 64)
+		if err != nil {
+			return nil, err
+		}
+		val = x
+	case bool:
+		val = 0
+		if v {
+			val = 1
+		}
+	default:
+		return nil, fmt.Errorf("%T can not be casted to integer", val)
+	}
+	return types.CreateInt(val), nil
+}
+
+func Float(args ...types.Primitive) (types.Primitive, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("invalid number of arguments")
+	}
+	var (
+		raw = slices.Fst(args).Raw()
+		val float64
+	)
+	switch v := raw.(type) {
+	case int64:
+		val = float64(v)
+	case float64:
+		val = v
+	case string:
+		x, err := strconv.ParseFloat(v, 64)
+		if err != nil {
+			return nil, err
+		}
+		val = x
+	case bool:
+		val = 0
+		if v {
+			val = 1
+		}
+	default:
+		return nil, fmt.Errorf("%T can not be casted to float", val)
+	}
+	return types.CreateFloat(val), nil
+}
+
+func String(args ...types.Primitive) (types.Primitive, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("invalid number of arguments")
+	}
+	str := slices.Fst(args).String()
+	return types.CreateString(str), nil
+}
+
+func Bool(args ...types.Primitive) (types.Primitive, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("invalid number of arguments")
+	}
+	return types.CreateBool(slices.Fst(args).True()), nil
 }
