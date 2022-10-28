2b556fa0fda5e1f1021338cd1672e7fb6468bf73
midbel <brun.nicolas21@gmail.com>
2022-10-28 19:41:48 +0200

    arithmetic on array

--- types/array.go
+++ types/array.go
@@ -46,8 +46,105 @@ func (a Array) True() bool {
 	return len(a.values) > 0
 }
 
+func (a Array) Rev() (Primitive, error) {
+	return nil, unsupportedOp("reverse", a)
+}
+
 func (a Array) Not() (Primitive, error) {
 	return CreateBool(!a.True()), nil
+}
+
+func (a Array) Add(other Primitive) (Primitive, error) {
+	switch x := other.(type) {
+	case Array:
+		a.values = append(a.values, x.values...)
+	default:
+		a.values = append(a.values, other)
+	}
+	return a, nil
+}
+
+func (a Array) Sub(other Primitive) (Primitive, error) {
+	var offset int
+	switch x := other.(type) {
+	case Int:
+		offset = int(x.value)
+	case Float:
+		offset = int(x.value)
+	default:
+		return nil, incompatibleType("multiply", a, other)
+	}
+	if offset > len(a.values) || -offset > len(a.values) {
+		a.values = []Primitive{}
+		return a, nil
+	}
+	if offset < 0 {
+		a.values = a.values[-offset:]
+	} else {
+		a.values = a.values[:len(a.values)-offset]
+	}
+	return a, nil
+}
+
+func (a Array) Div(other Primitive) (Primitive, error) {
+	var offset int
+	switch x := other.(type) {
+	case Int:
+		offset = int(x.value)
+	case Float:
+		offset = int(x.value)
+	default:
+		return nil, incompatibleType("multiply", a, other)
+	}
+	if offset <= 0 {
+		return nil, fmt.Errorf("array can not be divided negative values")
+	}
+	if offset > len(a.values) {
+		return nil, fmt.Errorf("array can not be divided by %d", offset)
+	}
+	var (
+		arr  Array
+		size = len(a.values)
+		step = size / offset
+	)
+	fmt.Println(step)
+	for i := 0; i < size && len(arr.values) < offset; i += step {
+		end := i + step
+		if end > size || len(arr.values) == offset-1 {
+			end = size
+		}
+		sub := a.values[i:end]
+		arr.values = append(arr.values, CreateArray(sub))
+	}
+	return arr, nil
+}
+
+func (a Array) Mul(other Primitive) (Primitive, error) {
+	var offset int
+	switch x := other.(type) {
+	case Int:
+		offset = int(x.value)
+	case Float:
+		offset = int(x.value)
+	default:
+		return nil, incompatibleType("multiply", a, other)
+	}
+	offset--
+
+	vs := make([]Primitive, len(a.values))
+	copy(vs, a.values)
+	for i := 0; i < offset; i++ {
+		a.values = append(a.values, vs...)
+	}
+	return a, nil
+}
+
+func (a Array) Pow(other Primitive) (Primitive, error) {
+	return nil, unsupportedOp("power", a)
+}
+
+func (a Array) Mod(other Primitive) (Primitive, error) {
+	return nil, unsupportedOp("modulo", a)
 }
 
 func (a Array) Set(ix, value Primitive) (Primitive, error) {
